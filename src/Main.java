8134091ed0df22fe21adc0c1d28beba51a963307
saverayash <yashsavera433@gmail.com>
2026-04-13 16:25:20 +0530

    want to learn rebase

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ class Main
 {
     public static void main(String[] args)
     {
+        System.out.println("first branch ");
+        System.out.println("YASh");
 
-        System.out.println("YASh");
-        System.out.println("YASh");
-        System.out.println("AAA");
-        System.out.println("savera");
     }
 }
